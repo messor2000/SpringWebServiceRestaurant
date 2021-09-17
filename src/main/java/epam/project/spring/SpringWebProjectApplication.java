023174872e15b012333bb396907db77cc133a25f
Alexandr Ovcharenko <ovcharenko.messor@gmail.com>
2021-09-17 19:19:47 +0300

    init

---
+++ src/main/java/epam/project/spring/SpringWebProjectApplication.java
@@ -0,0 +1,13 @@
+package epam.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epam/project/spring/SpringWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.project.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
